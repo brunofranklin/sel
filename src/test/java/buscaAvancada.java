bc1cae9649f914b77f9c00ed0d99a5e87a5be624
gabiramires <98050883+gabiramires@users.noreply.github.com>
2022-11-29 11:26:13 -0300

    testegabi

--- src/test/java/buscaAvancada.java
+++ src/test/java/buscaAvancada.java
@@ -4,32 +4,49 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class buscaAvancada {
-
-
+    ChromeDriver nav;
     @Test
-    public void filtroGrupos () {
-        System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver");
-        ChromeDriver nav = new ChromeDriver();
+    public void Main(){
+        System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
+        nav = new ChromeDriver();
+        login();
+        AcessoTenant();
+        TelaDevice();
+    }
+
+
+    public void login(){
+
 
         nav.get("https://app-test.pulsus.mobi");
         //nav.manage().window().maximize();
         nav.findElement(By.xpath("//*[@id=\"identifier\"]")).click();
         nav.findElement(By.xpath("//*[@id=\"identifier\"]")).click();
         //Login
-        nav.findElement(By.xpath("//*[@id=\"identifier\"]")).sendKeys("br@br.com");
+        nav.findElement(By.xpath("//*[@id=\"identifier\"]")).sendKeys("timeqa@pulsus.mobi");
         nav.findElement(By.xpath("//*[@id=\"password\"]")).click();
         //Password
-        nav.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("teste");
+        nav.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("QAbggi123");
         nav.findElement(By.xpath("//*[@id=\"action\"]")).click();
-        nav.findElement(By.xpath("//*[@id=\"theme-color\"]/div/span")).click();
+
+    }
+    public void AcessoTenant() {
+
         //Escolhe o Tenant
+        nav.findElement(By.xpath("//*[@id=\"theme-color\"]/div/span")).click();
         nav.findElement(By.xpath("//*[@id=\"theme-color\"]/div/span/div/a[1]")).click();
-        nav.findElement(By.xpath("//*[@id=\"root\"]/div/nav/div[7]/div[1]/a/span[2]")).click();
-        nav.findElement(By.xpath("//*[@id=\"root\"]/div/nav/div[7]/a[1]/span[2]")).click();
+        // nav.findElement(By.xpath("//*[@id=\"tenant-select\"]/div[2]/div/a[73]/div")).click();
+        nav.findElement(By.xpath("//*[@id=\"tenant-select\"]/div[2]/div/a[36]/div")).click();
+        nav.findElement(By.xpath("//*[@id=\"root\"]/div/nav/div[8]/div[1]/a/span[2]")).click();
+        //*[@id="root"]/div/nav/div[8]/div[1]/a/span[2]
+    }
+    public void TelaDevice() {
+
+
         // Entra no Iframe
-        nav.switchTo().frame("devices-new-iframe");
-        nav.findElement(By.id("advanced-search")).click();
-        nav.findElement(By.id("switch-group")).click();
+       nav.switchTo().frame("devices-new-iframe");
+       nav.findElement(By.id("advanced-search")).click();
+       nav.findElement(By.id("switch-group")).click();
       /*  nav.findElement(By.xpath("//*[@id=\"main-container\"]/main/section[1]/div[1]/div[2]/div/section/div/div[1]/div/input")).sendKeys("Grupo Teste");
         nav.findElement(By.xpath("//*[@id=\"2\"]/span/span/div")).click();
 
